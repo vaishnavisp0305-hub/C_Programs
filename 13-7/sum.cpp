344c59dd84dc84acbeab3b364cc3aeee93c31968
vaishnavisp0305-hub <vaishnavisp0305@gmail.com>
2026-08-08 18:17:50 +0000

    added sum.cpp

---
+++ 13-7/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x,y,s;
+    cout<<"Enter the first number : ";
+    cin>>x;
+    cout<<"Enter the second number : ";
+    cin>>y;
+    s=x+y;
+    cout<<s;
+    cout<<"\n";
+    return 0;
+}
