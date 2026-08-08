144f8585e56745af69b96dfb608b899df69d577b
vaishnavisp0305-hub <vaishnavisp0305@gmail.com>
2026-08-08 22:47:37 +0530

    Create factorial.cpp

---
+++ 13-7/factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namspace std;
+int main() {
+  int n,f=1,i;
+  cout<<'Enter a number: ";
+  cin>>n;
+  i=n;
+  while(i>0) {
+     f=f*i;
+     i--;
+  }
+  cout<<"Factorial of "<<n<<" is : "<<f<<endl;
+  return 0;
+}
