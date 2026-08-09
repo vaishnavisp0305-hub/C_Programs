dd1484420e5eddba0ce65237983b2a96acbe5632
vaishnavisp0305-hub <vaishnavisp0305@gmail.com>
2026-08-09 06:27:47 +0000

    sum and sub

---
+++ 20-7/file1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+namespace syc {
+    int show()
+    {
+        cout<<"Welcome syc";
+        cout<<"Perform C++ practical";
+        return 0;
+    }
+}
+int main() {
+    syc::show();
+    return 0;
+}
---
+++ 20-7/sum_sub.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+namespace vaishnavi {
+    int sum()
+     {
+        int a,b,add;
+        cout<<"Enter first number : ";
+        cin>>a;
+        cout<<"Enter second number : ";
+        cin>>b;
+        add=a+b;
+        return add;
+    }
+
+    int sub() 
+    {
+        int x,y,sub;
+        cout<<"Enter first number : ";
+        cin>>x;
+        cout<<"Enter second number : ";
+        cin>>y;
+        sub=x-y;
+        return sub;
+    }
+}
+int main() {
+    cout<<"Sum = "<<vaishnavi::sum()<<endl;
+    cout<<"Sub = "<<vaishnavi::sub()<<endl;
+    return 0;
+}
