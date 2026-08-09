f93e06ad095a963d9431cd1fcd0ecda2985830b9
vaishnavisp0305-hub <vaishnavisp0305@gmail.com>
2026-08-09 09:28:20 +0000

    manipulators

---
+++ 3-8/manipulator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    int Basic=950,Allowance=95,Total=545;
+    cout<<setw(5)<<"Basic"<<setw(5)<<Basic<<endl
+    <<setw(5)<<"Allowance"<<setw(5)<<Allowance<<endl
+    <<setw(5)<<"Total"<<setw(5)<<Total<<endl;
+    return 0;
+}
